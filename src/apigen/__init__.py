3b3a50cb7f6249e5bd3125c0fc400bc84957d82f
Ram Suthar <ramsuthar305@gmail.com>
2025-07-20 05:10:23 +0530

    updated project toml
    Signed-off-by: Ram Suthar <ramsuthar305@gmail.com>

---
+++ src/apigen/__init__.py
@@ -0,0 +1,11 @@
+"""
+APIGen - A Django API toolkit for response utilities, exceptions, middleware, and alert handlers.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Ram Suthar"
+__email__ = "ramsuthar305@gmail.com"
+
+# Import key components for easy access
+from .commons.base_response import *
+from .exceptions.api_exceptions import *
